f4b11023f7eca1760683e3cc855f1a0329603b88
Geoffrey Kleinberg <gkleinberg21@gmail.com>
2024-06-03 12:54:31 -0400

    added s-hat algorithm

--- graph.py
+++ graph.py
@@ -40,9 +40,78 @@ class Graph:
         while True:
             stable = True
             for v in self.graph:
-                if v not in F and len(self.get_neighbors(v).symmetric_difference(F)) == 1:
-                    F = F.union(self.get_neighbors(v))
+                neighbor_count_not_in_F = 0
+                neighbor_not_in_F = None
+                for neighbor in self.get_neighbors(v):
+                    if not neighbor in F:
+                        neighbor_count_not_in_F += 1
+                        neighbor_not_in_F = neighbor
+
+                if neighbor_count_not_in_F == 1:
+                    F.add(neighbor_not_in_F)
                     stable = False
+
             if stable:
                 break
+        return F
+    
+    def s_hat(self, initial_set=set(())):
+        for v in initial_set:
+            if v not in self.graph:
+                return None
+            
+        values = {}
+        for v in self.graph:
+            values[v] = None
+
+        F = self.skew_zero_forcing(initial_set)
+        
+        for v in F:
+            values[v] = 0
+            
+        while True:
+            this_round = set(())
+            for v in self.graph:
+                if values[v] is None:
+                    this_round.add(v)
+                    values[v] = 1
+                    break
+            
+            contradiction = False
+
+
+            while True:
+                stable2 = True
+                for v in self.graph:
+                    neighbors = self.get_neighbors(v)
+                    neighbor_sum = 0
+                    count_none_neighbors = 0
+                    none_neighbor = None
+                    for neighbor in neighbors:
+                        if values[neighbor] is None:
+                            count_none_neighbors += 1
+                            none_neighbor = neighbor
+                        else:
+                            neighbor_sum += values[neighbor]
+                    
+                    if count_none_neighbors == 1:
+                        values[none_neighbor] = -1 * neighbor_sum
+                        this_round.add(none_neighbor)
+                        stable2 = False
+
+                    if count_none_neighbors == 0 and neighbor_sum != 0:
+                        contradiction = True
+                        break
+                
+                if stable2:
+                    break
+
+            if contradiction:
+                for v in this_round:
+                    values[v] = 0
+                    F.add(v)
+
+            if list(values.values()).count(None) == 0:
+                break
+
         return F
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import graph
 
 def main():
     g = graph.Graph.make_from_file('graphs/c4.txt')
-    print(g.skew_zero_forcing(set(('A'))))
+    # print(g.skew_zero_forcing(set(('A'))))
+    print(g.s_hat(set(('C', 'D'))))
 
 
 if __name__ == '__main__':
